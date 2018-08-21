c5d63cd68cb34e18933125a2c2c0d2d736382c46
zhx198600 <zhx198600@126.com>
2018-08-21 22:16:10 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-!function() {
+/*!function() {
     // 自执行
-}();
+}();*/
 $(document).ready(function(){
-    $(".mobileMenuBtn").on("click", function(){
+    $('.mobileMenuBtn').on('click', function(){
         $('#mobileNav').addClass('mm-opened');
         $('#mobileNav').addClass('mm-current');
     });
+    $(window).scroll(function(){
+        var a = $(document).scrollTop();
+        if (a > 1200) {
+            $('.met-scroll-top').removeClass('hide');
+        } else {
+            $('.met-scroll-top').addClass('hide');
+        }
+    });
 });
+
+
