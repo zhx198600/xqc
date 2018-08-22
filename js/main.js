a7aba8e611998a17125f26215b915bfc5d34c997
zhx198600 <zhx198600@126.com>
2018-08-22 21:45:08 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@
     // 自执行
 }();*/
 $(document).ready(function(){
+    // 导航菜单
     $('.mobileMenuBtn').on('click', function(){
         $('#mobileNav').addClass('mm-opened');
         $('#mobileNav').addClass('mm-current');
     });
+    // 返回顶部
     $(window).scroll(function(){
         var a = $(document).scrollTop();
         if (a > 1200) {
@@ -14,6 +16,13 @@ $(document).ready(function(){
             $('.met-scroll-top').addClass('hide');
         }
     });
+    $('.met-scroll-top').on('click', function(){
+        $('html,body').animate({
+            scrollTop: '0px'
+        }, 800);
+        return false;
+    });
+    // 产品列表tab页切换
     $('.product-class li').on('click', function () {
         $('.tabcon').addClass('hide');
         $('.product-class li').removeClass('active');
